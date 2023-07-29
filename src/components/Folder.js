3b684284be13c90da2e75f10cd90367c908527a3
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-07-29 22:40:59 +0600

    Updated

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,5 +1,25 @@
-function Folder() {
-    return <div>I am Folder</div>;
+import { useState } from "react";
+
+function Folder({ explorer }) {
+    const [expand, setExpand] = useState(false);
+
+    if (explorer.isFolder) {
+        return (
+            <div>
+                <span onClick={ () => setExpand(!expand) }>{ explorer.name } <br /></span>
+                
+                <div style={{ display: expand ? "block" : "none", paddingLeft: 15 }}>
+                    {explorer.items.map(exp => {
+                        return <Folder explorer={exp} />;
+                        })}
+                        
+                </div>
+            </div>
+        );
+    } else {
+       return <span>{ explorer.name }</span>;
+    }
+    
 }
 
 export default Folder;
